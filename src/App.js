07669bd66d55567fd7318e80819d76000e56e097
mxwll <mxwllndrsn@gmail.com>
2024-01-11 00:24:24 -0800

    remove calendar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
-import Calendar from 'react-calendar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        xlk
+          <p>
+            xlk
+          </p>
+          <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <Calendar />
     </div>
   );
 }
