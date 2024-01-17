7a308b60068029e8bfdbd98990d3d49922e86f41
mxwll <mxwllndrsn@gmail.com>
2024-01-16 23:22:51 -0800

    amplify signin

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
           <p>
             xlk
           </p>
+          <Authenticator>
+            {({ signOut, user }) => (
+              <main>
+                <h1>hello { user.username }</h1>
+                <button onClick={ signOut }>sign out</button>
+              </main>
+            )}
+          </Authenticator>
           <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
 }
-
-export default App;
