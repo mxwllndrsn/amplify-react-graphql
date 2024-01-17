b3bb4de31bf8698942599e9471357123337eddd3
mxwll <mxwllndrsn@gmail.com>
2024-01-17 00:00:15 -0800

    withAuthenticator

--- src/App.js
+++ src/App.js
@@ -2,28 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
 
 Amplify.configure(awsExports);
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
           <p>
             xlk <img src={logo} className="App-logo" alt="logo" />
           </p>
-          <Authenticator>
+          <withAuthenticator>
             {({ signOut, user }) => (
               <main>
                 <h1>hello { user.username }</h1>
                 <button onClick={ signOut }>sign out</button>
               </main>
             )}
-          </Authenticator>
+          </withAuthenticator>
       </header>
     </div>
   );
 }
+
+export default withAuthenticator(App);
